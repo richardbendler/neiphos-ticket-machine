bf1ad8fc960784b3e8751f93e2ebaa51f1001cf1
chinberdradler <chinberdradler@gmail.com>
2026-08-09 00:01:57 +0200

    fix: Huepftierbilder freigestellt (transparentes PNG statt Hintergrund-JPG)
    Der bisherige fast-weisse Studio-Hintergrund war als JPG fest mit
    eingebrannt -- an Stellen, wo die Bilder klein und mit einem eigenen
    Symbol daneben/darauf dargestellt werden (z. B. Mini Metro), sah das wie
    ein Darstellungsfehler aus. Alle 21 bestehenden Fotos per Flood-Fill vom
    Bildrand freigestellt (mit Saettigungs-Gate, damit der Umriss auch bei
    farbigen Verlaufshintergruenden korrekt haelt) und dabei gleich 10 neu
    recherchierte Tierarten (Biene, Frosch, Nilpferd, Rentier, ...) ergaenzt.

--- src/data/hopperAnimals.ts
+++ src/data/hopperAnimals.ts
@@ -2,7 +2,8 @@
  * Alternatives Bildthema zu den Zugfotos (data/trains.ts): "Rody"-artige
  * Sitz-Hüpftiere (Hartplastik-Hüpfball mit Tiergesicht, zwei Ohren UND vier
  * kurzen Beinen zum Draufsitzen/Hüpfen) statt Zügen -- fuer den optionalen
- * Hüpftier-Modus bei Zug-Spotter und Zug-Memory.
+ * Hüpftier-Modus bei Zug-Spotter und Zug-Memory, sowie fuer Passagiere
+ * zählen, Mini Metro, Zugfoto und Hüpftier-Glücksspiel.
  *
  * Bewusst nur EIN Spielzeug-Typ (der "Rody"-Bauart mit Beinen): eine erste
  * Version enthielt zusaetzlich den klassischen "Space Hopper" (Kugel mit
@@ -25,29 +26,47 @@
  * an den Raendern blieb dadurch jeweils ein sichtbarer Rest des jeweils
  * anderen Elefanten stehen (gemeldet, sah dadurch schlecht/unfertig aus).
  * "elephant-gray" ist ein eigenstaendiges, sauberes Foto und bleibt erhalten.
+ *
+ * Alle Bilder liegen als transparente PNGs vor (per Flood-Fill vom Bildrand
+ * freigestellt, siehe scratch-Skript der Bearbeitungs-Session -- die
+ * urspruenglichen Fotos hatten einen fast-weissen/-grauen Studio-Hintergrund
+ * als JPG einfach mit eingebrannt). Wichtig, weil die Bilder in mehreren
+ * Spielen klein UND mit einem eigenen Formsymbol daneben/darauf dargestellt
+ * werden (z. B. Mini Metro) -- ein sichtbarer heller Kasten drumherum sah
+ * dort wie ein Darstellungsfehler aus.
  */
-import dogTealUrl from "../assets/images/hoppers/hopper-dog-teal.jpg";
-import caterpillarGreenUrl from "../assets/images/hoppers/hopper-caterpillar-green.jpg";
-import horseBlueUrl from "../assets/images/hoppers/hopper-horse-blue.jpg";
-import horseBrownUrl from "../assets/images/hoppers/hopper-horse-brown.jpg";
-import dinoGreenUrl from "../assets/images/hoppers/hopper-dino-green.jpg";
-import dogPurpleUrl from "../assets/images/hoppers/hopper-dog-purple.jpg";
-import dinoTealUrl from "../assets/images/hoppers/hopper-dino-teal.jpg";
-import dogBrownUrl from "../assets/images/hoppers/hopper-dog-brown.jpg";
-import dinoOrangeUrl from "../assets/images/hoppers/hopper-dino-orange.jpg";
-import trexGreenUrl from "../assets/images/hoppers/hopper-trex-green.jpg";
-import unicornPurpleUrl from "../assets/images/hoppers/hopper-unicorn-purple.jpg";
-import dinoGrayUrl from "../assets/images/hoppers/hopper-dino-gray.jpg";
-import giraffeYellowUrl from "../assets/images/hoppers/hopper-giraffe-yellow.jpg";
-import cowWhiteUrl from "../assets/images/hoppers/hopper-cow-white.jpg";
-import cowPinkUrl from "../assets/images/hoppers/hopper-cow-pink.jpg";
-import cowGreenUrl from "../assets/images/hoppers/hopper-cow-green.jpg";
-import rabbitCreamUrl from "../assets/images/hoppers/hopper-rabbit-cream.jpg";
-import elephantGrayUrl from "../assets/images/hoppers/hopper-elephant-gray.jpg";
-import horseRedUrl from "../assets/images/hoppers/hopper-horse-red.jpg";
-import unicornWhiteUrl from "../assets/images/hoppers/hopper-unicorn-white.jpg";
-import foxOrangeUrl from "../assets/images/hoppers/hopper-fox-orange.jpg";
-import llamaBrownUrl from "../assets/images/hoppers/hopper-llama-brown.jpg";
+import dogTealUrl from "../assets/images/hoppers/hopper-dog-teal.png";
+import caterpillarGreenUrl from "../assets/images/hoppers/hopper-caterpillar-green.png";
+import caterpillarMintUrl from "../assets/images/hoppers/hopper-caterpillar-mint.png";
+import horseBlueUrl from "../assets/images/hoppers/hopper-horse-blue.png";
+import horseBrownUrl from "../assets/images/hoppers/hopper-horse-brown.png";
+import horseChestnutUrl from "../assets/images/hoppers/hopper-horse-chestnut.png";
+import horseCrimsonUrl from "../assets/images/hoppers/hopper-horse-crimson.png";
+import dinoGreenUrl from "../assets/images/hoppers/hopper-dino-green.png";
+import dogPurpleUrl from "../assets/images/hoppers/hopper-dog-purple.png";
+import dogGrayUrl from "../assets/images/hoppers/hopper-dog-gray.png";
+import dinoTealUrl from "../assets/images/hoppers/hopper-dino-teal.png";
+import dogBrownUrl from "../assets/images/hoppers/hopper-dog-brown.png";
+import dinoOrangeUrl from "../assets/images/hoppers/hopper-dino-orange.png";
+import trexGreenUrl from "../assets/images/hoppers/hopper-trex-green.png";
+import unicornPurpleUrl from "../assets/images/hoppers/hopper-unicorn-purple.png";
+import unicornPinkUrl from "../assets/images/hoppers/hopper-unicorn-pink.png";
+import dinoGrayUrl from "../assets/images/hoppers/hopper-dino-gray.png";
+import giraffeYellowUrl from "../assets/images/hoppers/hopper-giraffe-yellow.png";
+import giraffeBrownUrl from "../assets/images/hoppers/hopper-giraffe-brown.png";
+import cowWhiteUrl from "../assets/images/hoppers/hopper-cow-white.png";
+import cowPinkUrl from "../assets/images/hoppers/hopper-cow-pink.png";
+import cowGreenUrl from "../assets/images/hoppers/hopper-cow-green.png";
+import rabbitCreamUrl from "../assets/images/hoppers/hopper-rabbit-cream.png";
+import elephantGrayUrl from "../assets/images/hoppers/hopper-elephant-gray.png";
+import horseRedUrl from "../assets/images/hoppers/hopper-horse-red.png";
+import unicornWhiteUrl from "../assets/images/hoppers/hopper-unicorn-white.png";
+import foxOrangeUrl from "../assets/images/hoppers/hopper-fox-orange.png";
+import llamaBrownUrl from "../assets/images/hoppers/hopper-llama-brown.png";
+import beeYellowUrl from "../assets/images/hoppers/hopper-bee-yellow.png";
+import reindeerGrayUrl from "../assets/images/hoppers/hopper-reindeer-gray.png";
+import frogGreenUrl from "../assets/images/hoppers/hopper-frog-green.png";
+import hippoPinkUrl from "../assets/images/hoppers/hopper-hippo-pink.png";
 
 export interface HopperAnimalCard {
   id: string;
@@ -58,17 +77,23 @@ export interface HopperAnimalCard {
 export const hopperAnimalCards: HopperAnimalCard[] = [
   { id: "dog-teal", name: "Hüpftier-Hund (türkis)", image: dogTealUrl },
   { id: "caterpillar-green", name: "Hüpftier-Raupe (grün)", image: caterpillarGreenUrl },
+  { id: "caterpillar-mint", name: "Hüpftier-Raupe (mint)", image: caterpillarMintUrl },
   { id: "horse-blue", name: "Hüpftier-Pferd (blau)", image: horseBlueUrl },
   { id: "horse-brown", name: "Hüpftier-Pferd (braun)", image: horseBrownUrl },
+  { id: "horse-chestnut", name: "Hüpftier-Pferd (kastanienbraun)", image: horseChestnutUrl },
+  { id: "horse-crimson", name: "Hüpftier-Pferd (karmesinrot)", image: horseCrimsonUrl },
   { id: "dino-green", name: "Hüpftier-Dino (grün)", image: dinoGreenUrl },
   { id: "dog-purple", name: "Hüpftier-Hund (lila)", image: dogPurpleUrl },
+  { id: "dog-gray", name: "Hüpftier-Hund (grau)", image: dogGrayUrl },
   { id: "dino-teal", name: "Hüpftier-Dino (türkis)", image: dinoTealUrl },
   { id: "dog-brown", name: "Hüpftier-Hund (braun)", image: dogBrownUrl },
   { id: "dino-orange", name: "Hüpftier-Dino (orange)", image: dinoOrangeUrl },
   { id: "trex-green", name: "Hüpftier-T-Rex (grün)", image: trexGreenUrl },
   { id: "unicorn-purple", name: "Hüpftier-Einhorn (lila)", image: unicornPurpleUrl },
+  { id: "unicorn-pink", name: "Hüpftier-Einhorn (rosa)", image: unicornPinkUrl },
   { id: "dino-gray", name: "Hüpftier-Dino (grau)", image: dinoGrayUrl },
   { id: "giraffe-yellow", name: "Hüpftier-Giraffe (gelb)", image: giraffeYellowUrl },
+  { id: "giraffe-brown", name: "Hüpftier-Giraffe (braun)", image: giraffeBrownUrl },
   { id: "cow-white", name: "Hüpftier-Kuh (weiß)", image: cowWhiteUrl },
   { id: "cow-pink", name: "Hüpftier-Kuh (pink)", image: cowPinkUrl },
   { id: "cow-green", name: "Hüpftier-Kuh (grün)", image: cowGreenUrl },
@@ -78,4 +103,8 @@ export const hopperAnimalCards: HopperAnimalCard[] = [
   { id: "unicorn-white", name: "Hüpftier-Einhorn (weiß)", image: unicornWhiteUrl },
   { id: "fox-orange", name: "Hüpftier-Fuchs (orange)", image: foxOrangeUrl },
   { id: "llama-brown", name: "Hüpftier-Lama (braun)", image: llamaBrownUrl },
+  { id: "bee-yellow", name: "Hüpftier-Biene (gelb)", image: beeYellowUrl },
+  { id: "reindeer-gray", name: "Hüpftier-Rentier (grau)", image: reindeerGrayUrl },
+  { id: "frog-green", name: "Hüpftier-Frosch (grün)", image: frogGreenUrl },
+  { id: "hippo-pink", name: "Hüpftier-Nilpferd (pink)", image: hippoPinkUrl },
 ];
